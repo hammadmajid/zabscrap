bd184255059d51c2d401b361e7e05c5f52550878
Hammad Majid <hammadmajid@proton.me>
2026-03-10 03:31:23 +0500

    v4

--- main.go
+++ main.go
@@ -23,37 +23,36 @@ func main() {
 		Jar: jar,
 	}
 
-	// Step 1: Login
+	// Session Login
 	loginURL := "https://springzabdesk.szabist-isb.edu.pk/VerifyLogin.asp?sid=974494673"
-	data := url.Values{}
-	data.Set("txtLoginName", username)
-	data.Set("txtPassword", password)
-	data.Set("txtCampus_Id", "1")
+	loginData := url.Values{}
+	loginData.Set("txtLoginName", username)
+	loginData.Set("txtPassword", password)
+	loginData.Set("txtCampus_Id", "1")
 
-	resp, err := client.PostForm(loginURL, data)
+	resp, err := client.PostForm(loginURL, loginData)
 	if err != nil {
-		fmt.Printf("Login Error: %v\n", err)
 		return
 	}
 	resp.Body.Close()
 
-	// Step 2: Access Attendance Page
-	// Note: Use the SID from your previous successful redirect
+	// Submit form for CSC 2205 Operating Systems
 	attendanceURL := "https://springzabdesk.szabist-isb.edu.pk/Student/QryCourseAttendance.asp?OptionName=View%20Attendance&sid=974494733"
 
-	resp, err = client.Get(attendanceURL)
+	formData := url.Values{}
+	formData.Set("txtFac", "Fakhar")
+	formData.Set("txtSem", "16227")
+	formData.Set("txtSec", "3")
+	formData.Set("txtCou", "2726")
+
+	resp, err = client.PostForm(attendanceURL, formData)
 	if err != nil {
-		fmt.Printf("Attendance Fetch Error: %v\n", err)
 		return
 	}
 	defer resp.Body.Close()
 
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Printf("Read Error: %v\n", err)
-		return
-	}
+	body, _ := io.ReadAll(resp.Body)
 
-	fmt.Println("--- Attendance Page Content ---")
+	fmt.Println("--- Course Attendance Detail Content ---")
 	fmt.Println(string(body))
 }
